192b143221432db3c6adbf0df2359e614c72fe7f
abdo <abdo2000.am@gmail.com>
2023-10-12 17:03:57 -0700

    aa

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef main_h
 #define main_h
+int _putchar();
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
