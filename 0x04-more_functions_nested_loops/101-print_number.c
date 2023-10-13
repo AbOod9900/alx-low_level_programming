bde1a57635adc2ad20d3bfd4cc50aa635df29206
abdo <abdo2000.am@gmail.com>
2023-10-12 18:55:36 -0700

    my commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,7 @@
  * Return: Always 0.
  */
 void print_number(int n)
+
 {
 	unsigned int z;
 	int m, b;
@@ -23,10 +24,10 @@ void print_number(int n)
 		_putchar (n + '0');
 	}
 
-else if (n < 0)
-		{	n = n * -1;
+	else if (n < 0)
+	{	n = n * -1;
 			_putchar ('-');
-		}
+	}
 		z = n;
 	while (z / b > 9)
 	{
@@ -35,10 +36,11 @@ else if (n < 0)
 	while (b > 0)
 	{
 
+	}
+
 		m = z / b;
 		z = z % b;
 		_putchar (m + '0');
 		b = b / 10;
 
-}
 }
